0ea25e4399c7f375ae0ab5e919228b609c0860d5
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-08-18 19:18:41 +0530

    handling mouse up mouse down and mouse enter events

--- src/components/SelectableGrid.jsx
+++ src/components/SelectableGrid.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function SelectableGrid({rows=10, cols=10}) {
-  console.log(rows , cols)
+function SelectableGrid({ rows = 10, cols = 10 }) {
+  // console.log(rows , cols)
+  const [isMouseDown, setIsMouseDown] = useState(false);
+
+  function handleMouseDown(index) {
+    setIsMouseDown(true)
+    console.log(index)
+  }
+
+  function handleMouseUp(index) {
+    setIsMouseDown(false)
+    console.log(index)
+  }
+
+  function handleMouseEnter(index) {
+    if (setIsMouseDown)
+      console.log(index)
+  }
   return (
-    <div className='grid' style={{"--rows": rows, "---cols":cols}}>
+    <div className='grid' style={{ "--rows": rows, "---cols": cols }} >
       {
-        [...Array(rows*cols).keys()].map((_,i)=> <div className='box'> 
-          {i+1}
+        [...Array(rows * cols).keys()].map((_, i) => <div key={i} className='box' onMouseDown={() => handleMouseDown(i + 1)} onMouseEnter={() => handleMouseEnter(i + 1)} onMouseUp={() => handleMouseUp(i + 1)}>
+          {i + 1}
         </div>)
       }
 
